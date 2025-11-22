4ed7e2a96b8af3811380ee9e127be84db8e15939
Abel Velasquez <92559628+abelvelasquezchavez@users.noreply.github.com>
2025-11-22 02:14:37 -0500

    escuchamos multiples provider simultaneamente

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -36,6 +36,9 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
---
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      'Cargando películas',
+      'Comprando palomitas de maiz',
+      'Cargando populares',
+      'Llamando a mi novia',
+      'Ya mero...',
+      'Esto esta tardando mas de lo esperado',
+    ];
+    return Stream.periodic(const Duration(milliseconds: 1200), (step) {
+      return messages[step];
+    }).take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Cargando...'),
+          const SizedBox(height: 10),
+          const CircularProgressIndicator(strokeWidth: 2),
+          const SizedBox(height: 10),
+          StreamBuilder<String>(
+            stream: getLoadingMessages(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text('Cargando...');
+              return Text(snapshot.data!);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

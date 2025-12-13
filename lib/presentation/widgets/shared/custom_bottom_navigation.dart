4eb7808c528cd0aca4395357f3c845450c7e9af3
Abel Velasquez <92559628+abelvelasquezchavez@users.noreply.github.com>
2025-12-13 16:01:36 -0500

    fix: errores de navegación y bottom navigation

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -4,25 +4,20 @@ import 'package:go_router/go_router.dart';
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
-void int getCurrentIndex(BuildContext context) {
-   final String location = GoRouterState.of(context).location;
-   
-   switch (location) {
-     case '/':
-       return 0;
-     case '/categories':
-       return 1;
-     case '/favorites':
-       return 2;
-     default:
-       return 0;
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).location;
 
-
-   }}
-
-
-
-
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
 
   void onItemTapped(BuildContext context, int index) {
     switch (index) {
@@ -42,6 +37,7 @@ void int getCurrentIndex(BuildContext context) {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
       onTap: (value) => onItemTapped(context, value),
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
